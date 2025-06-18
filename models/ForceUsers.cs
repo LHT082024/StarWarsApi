bbf89e60ef1d991b3dc09ba748bd1e9624b59e65
LHT082024 <LHT082024@gmail.com>
2025-06-18 09:41:14 +0200

    force user modelclass

---
+++ models/ForceUsers.cs
@@ -0,0 +1,13 @@
+namespace StarWarsApi.models
+{
+    public class ForceUsers
+    {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+        public string? LightsaberImg { get; set; }
+        public string? ForcePower1 { get; set; }
+        public string? ForcePower2 { get; set; }
+        public string? ForcePower3 { get; set; }
+
+    }
+}
