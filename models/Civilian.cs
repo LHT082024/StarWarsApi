448e3c04cbaad16c8f56c253e88c368f1d1b2d71
LHT082024 <LHT082024@gmail.com>
2025-06-18 09:48:53 +0200

    civilian model

---
+++ models/Civilian.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWarsApi.models
+{
+    public class Civilian
+    {
+        public string? Name { get; set; }
+        public string? ChImg { get; set; }
+        public string? Bio { get; set; }
+        public int Id { get; set; }
+    }
+}
